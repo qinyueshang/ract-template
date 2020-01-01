5e1f0b82961d31a25142234513fe981f65fa3e11
lehy <yue_hong_y@163.com>
2020-01-01 13:49:09 +0800

    modified

---
+++ init.js
@@ -0,0 +1,19 @@
+const fs = require("fs-extra");
+const path = require("path");
+const chalk = require("chalk");
+
+const spawn = require("cross-spawn");
+
+module.exports = function(appPath, appName, verbose, originalDirectory, templateName) {
+    const appPackage = require(path.join(appPath, "package.json"));
+    const ownPackageName = require(path.join(__dirname, ".", "package.json")).name;
+    const templatePath = path.join(appPath, "node_modules", ownPackageName);
+    const templateDir = path.join(templatePath, "template");
+
+    appPackage.name = appName;
+
+    fs.writeFileSync(path.join(appPath, "package.json"), JSON.stringify(appPackage, null, 2));
+    if (fs.existsSync(templateDir)) {
+        fs.copySync(templateDir, appPath);
+    }
+};
