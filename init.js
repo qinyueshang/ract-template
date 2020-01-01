691857e8dbbabbf417d7cfad0958e32fdd7ec4d8
lehy <yue_hong_y@163.com>
2020-01-01 14:14:19 +0800

    modified

--- init.js
+++ init.js
@@ -4,16 +4,28 @@ const chalk = require("chalk");
 
 const spawn = require("cross-spawn");
 
-module.exports = function(appPath, appName, verbose, originalDirectory, templateName) {
-    const appPackage = require(path.join(appPath, "package.json"));
-    const ownPackageName = require(path.join(__dirname, ".", "package.json")).name;
-    const templatePath = path.join(appPath, "node_modules", ownPackageName);
-    const templateDir = path.join(templatePath, "template");
+module.exports = function(
+  appPath,
+  appName,
+  verbose,
+  originalDirectory,
+  templateName
+) {
+  const appPackage = require(path.join(appPath, "package.json"));
+  const ownPackageName = require(path.join(__dirname, "..", "package.json"))
+    .name;
+  const templatePath = path.join(appPath, "node_modules", ownPackageName);
+  const templateDir = path.join(templatePath, "template");
+  const templateJson = require(path.join(templatePath, "template.json"));
 
-    appPackage.name = appName;
-
-    fs.writeFileSync(path.join(appPath, "package.json"), JSON.stringify(appPackage, null, 2));
-    if (fs.existsSync(templateDir)) {
-        fs.copySync(templateDir, appPath);
-    }
+  templateJson.name = appName;
+  templateJson.version = appPackage.version;
+  templateJson.description = appPackage.description;
+  fs.writeFileSync(
+    path.join(appPath, "package.json"),
+    JSON.stringify(templateJson, null, 2)
+  );
+  if (fs.existsSync(templateDir)) {
+    fs.copySync(templateDir, appPath);
+  }
 };
