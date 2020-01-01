ecaebe176a46499f822677dc838ccb5cd2918b70
lehy <yue_hong_y@163.com>
2020-01-01 15:01:44 +0800

    modified

--- scripts/init.js
+++ scripts/init.js
@@ -4,16 +4,47 @@ const chalk = require("chalk");
 
 const spawn = require("cross-spawn");
 
-module.exports = function(appPath, appName, verbose, originalDirectory, templateName) {
-    const appPackage = require(path.join(appPath, "package.json"));
-    const ownPackageName = require(path.join(__dirname, ".", "package.json")).name;
-    const templatePath = path.join(appPath, "node_modules", ownPackageName);
-    const templateDir = path.join(templatePath, "template");
+module.exports = function(
+  appPath,
+  appName,
+  verbose,
+  originalDirectory,
+  templateName
+) {
+  //   const appPackage = require(path.join(appPath, "package.json"));
+  //   const ownPackageName = require(path.join(__dirname, "..", "package.json"))
+  //     .name;
+  //   const templatePath = path.join(appPath, "node_modules", ownPackageName);
+  //   const templateDir = path.join(templatePath, "template");
 
-    appPackage.name = appName;
+  //   appPackage.name = appName;
 
-    fs.writeFileSync(path.join(appPath, "package.json"), JSON.stringify(appPackage, null, 2));
-    if (fs.existsSync(templateDir)) {
-        fs.copySync(templateDir, appPath);
-    }
+  //   fs.writeFileSync(
+  //     path.join(appPath, "package.json"),
+  //     JSON.stringify(appPackage, null, 2)
+  //   );
+  //   if (fs.existsSync(templateDir)) {
+  //     fs.copySync(templateDir, appPath);
+  //   }
+
+  const ownPackageName = require(path.join(__dirname, "..", "package.json"))
+    .name;
+  const ownPath = path.join(appPath, "node_modules", ownPackageName);
+  const appPackage = require(path.join(appPath, "package.json"));
+  const templateDependencies = require(path.join(
+    ownPath,
+    ".template.dependencies.json"
+  ));
+
+  Object.keys(templateDependencies).map(key => {
+    appPackage[key] = templateDependencies[key];
+  });
+
+  fs.writeFileSync(
+    path.join(appPath, "package.json"),
+    JSON.stringify(appPackage, null, 2)
+  );
+
+  const templatePath = path.join(ownPath, "template");
+  fs.copySync(templatePath, appPath);
 };
